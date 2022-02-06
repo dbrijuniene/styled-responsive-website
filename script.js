571544094c7763db93591e14bce753c011a7cc0f
dbrijuniene <liutkute.deimante@gmail.com>
2022-02-07 00:19:52 +0200

    implemented burger

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let burger = document.getElementById("burger");
+let navigationPanel = document.getElementById("navigationPanel");
+let actions = document.getElementsByTagName("a");
+
+for (const a of actions) {
+    a.addEventListener("click", function () {
+        navigationPanel.classList.toggle("expandet")
+    })
+}
+
+burger.addEventListener("click", function () {
+    navigationPanel.classList.toggle("expandet")
+})
