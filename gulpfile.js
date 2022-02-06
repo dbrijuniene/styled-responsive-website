5816abf540dbd69e0c4c68521e4ae58a5b790fb4
dbrijuniene <liutkute.deimante@gmail.com>
2022-02-07 01:14:19 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('serve', gulp.series(function(done) {
+gulp.task('serve', gulp.series(function (done) {
 
     browserSync.init({
         server: {
@@ -18,18 +18,18 @@ gulp.task('serve', gulp.series(function(done) {
 }));
 
 
-gulp.task('sass', gulp.series(function(done) {
-	return gulp.src('scss/style.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-		    .pipe(gulp.dest('app/css'))
+gulp.task('sass', gulp.series(function (done) {
+    return gulp.src('scss/style.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream());
-        done();
+    done();
 }));
 
 gulp.task('default', gulp.parallel('serve', function () {
